bb14fffebda8271a331a3ee104533e38ab05da28
Richmond Eribo <uyiosaeribo344@gmail.com>
2023-02-21 20:33:31 +0100

    update head meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,19 @@ const Head = () => {
   return (
     <>
       <title>News Site</title>
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta
+        name="description"
+        content="A News site that showcase the features of nextjs13 app directory"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="The News Site" />
+      <meta
+        property="og:description"
+        content="A News site that showcase the features of nextjs13 app directory"
+      />
+      <meta property="og:url" content="www.everydaynews.vercel.app" />
+      <meta property="og:image" content="/thenewssite.jpeg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </>
   )
 }
