4ba0196fbd23516887680f8adc22cda89f3c38db
Richmond Eribo <uyiosaeribo344@gmail.com>
2023-09-19 15:38:20 +0100

    edit images metadata

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -73,7 +73,7 @@ export async function generateMetadata({
       siteName: "Naija Metro News",
       images: [
         {
-          url: `https://naijametronews.com/${newsPost.thumbnail.url}`,
+          url: `https:${newsPost.thumbnail.url}`,
           width: 800,
           height: 600,
         },
@@ -86,7 +86,7 @@ export async function generateMetadata({
       card: "summary_large_image",
       title: newsPost.title,
       description: newsPost.excerpt,
-      images: [`https://naijametronews.com/${newsPost.thumbnail.url}`],
+      images: [`https:${newsPost.thumbnail.url}`],
     },
     robots: {
       index: true,
